1410226832fbe883a6fe8d08d58350c1be9cdb8b
Syed Muhammed Bilal <intergo878@gmail.com>
2020-06-19 18:32:27 +0500

    Update IncomeExpenses.js

--- src/component/IncomeExpenses.js
+++ src/component/IncomeExpenses.js
@@ -5,7 +5,7 @@ function IncomeExpenses() {
     const { transactions } = useContext(GlobalContext);
 
     const amounts = transactions.map(transaction => transaction.amount);
-    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    //const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
     const income = amounts
         .filter(item => item > 0)
